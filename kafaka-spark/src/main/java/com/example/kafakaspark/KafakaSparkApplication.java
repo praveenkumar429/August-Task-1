4a778931f174798c45933cb2f2d80448de4d3319
abhilashranjan <ranjan.abhilash@gmail.com>
2017-02-06 08:12:25 +0530

    initial commit

---
+++ kafaka-spark/src/main/java/com/example/kafakaspark/KafakaSparkApplication.java
@@ -0,0 +1,12 @@
+//package com.example.kafakaspark;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class KafakaSparkApplication {
+//
+////	public static void main(String[] args) {
+////		SpringApplication.run(KafakaSparkApplication.class, args);
+////	}
+//}
